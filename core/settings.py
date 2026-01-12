69989bb0b777661cef12b4e1f92a70ec01a177d4
maxim1976 <maximdongurool@gmail.com>
2026-01-12 12:10:58 +0800

    Update S3 bucket name to amz-real-max

--- core/settings.py
+++ core/settings.py
@@ -149,7 +149,7 @@ if USE_S3:
     # AWS Settings
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'demo-backet-real')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'amz-real-max')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-northeast-1')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     
