30debd82939924259bdde578c8bc582cde31a6c1
maxim1976 <maximdongurool@gmail.com>
2026-01-12 19:09:18 +0800

    Add AWS S3 storage configuration for media files

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -1,7 +1,14 @@
+"""
+Custom storage backends for AWS S3
+"""
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Storage backend for media files (user uploads)
+    Files will be stored in the 'media/' prefix in S3
+    """
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
